059f9ce2b39233a1838f5b338d6837f829ec7bd0
DoraB3mon <fan_yukun1996@163.com>
2021-04-28 00:31:23 +1000

    update on sign in function and create database

--- app.py
+++ app.py
@@ -47,27 +47,6 @@ def create_all():
 # def load_user(id):
 #     return UserModel.query.get(int(id))
 
-
-
-# @dataclass
-# class User:
-#     id: int
-#     username: str
-#     password: str
-
-# users = [
-# 	User(1, "Admin", "123456"),
-# 	User(2, "Eason", "888888"),
-# 	User(3, "Tommy", "666666"),
-# ]
-
-# @app.before_request
-# def before_request():
-#     g.user = None
-#     if 'user_id' in session:
-#         user = [u for u in users if u.id == session['user_id']][0] #todo 替换成数据库
-#         g.user = user
-
 #路由主页
 @app.route("/")
 def homepage():           
@@ -94,26 +73,6 @@ def activitylog():
 @app.route("/profile")
 def profile():        
     return render_template("profile.html")
-
-# @app.route("/login", methods=['GET', 'POST'])
-# def login():
-#     if g.user:
-#         return redirect(url_for('profile'))
-
-#     if request.method == 'POST':
-#         # login
-#         session.pop('user_id', None)
-#         username = request.form.get("username", None)
-#         password = request.form.get("password", None)
-#         print(username)
-#         user = [u for u in users if u.username==username] #todo 替换成数据库
-#         if len(user) > 0:
-#             user = user[0]
-#         if user and user.password == password:
-#             session['user_id'] = user.id
-#             return redirect(url_for('workoutRec'))
-        
-#     return render_template("sign.html")
 
 # @app.route('/login', methods=['GET', 'POST'])
 # def login():
